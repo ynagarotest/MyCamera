4496f9196c3d1ea4363b85f43a7eb2b0e6e821b4
Yasuo Nagaro <ynagaro@google.com>
2025-06-20 14:42:16 +0900

    Initial Commit

---
+++ MyCamera/MyCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCameraApp.swift
+//  MyCamera
+//
+//  Created by Yasuo Nagaro on 2025/06/20.
+//
+
+import SwiftUI
+
+@main
+struct MyCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
