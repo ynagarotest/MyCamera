4423b9db175aee7748d5108506a89300b71b420a
Yasuo Nagaro <ynagaro@google.com>
2025-06-23 18:11:06 +0900

    commit

---
+++ MyCamera/ImagePickerView.swift
@@ -0,0 +1,18 @@
+//
+//  ImagePickerView.swift
+//  MyCamera
+//
+//  Created by Yasuo Nagaro on 2025/06/23.
+//
+
+import SwiftUI
+
+struct ImagePickerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ImagePickerView()
+}
